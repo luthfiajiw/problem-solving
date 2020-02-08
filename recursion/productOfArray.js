c282cdf9508ace2e6dd29efb05c424579cc5d755
Luthfi Aji W <luthfi.ajiw@gmail.com>
2020-02-08 20:44:39 +0700

    prodcutOfArray

--- recursion/productOfArray.js
+++ recursion/productOfArray.js
@@ -3,8 +3,9 @@ function productOfArray(arr) {
   if(arr.length == 1) return arr[0];
   total = arr[0];
   arr.shift();
-  
+
   return total * productOfArray(arr);
 }
 
-console.log(productOfArray([1,2,3]));
+productOfArray([1,2,3]);
+// result = 6
