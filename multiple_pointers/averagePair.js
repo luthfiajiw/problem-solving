888e4531ccad21e8b113b0734653544cc0c9e8d0
Luthfi Aji W <luthfi.ajiw@gmail.com>
2020-02-08 16:42:18 +0700

    averagePair

---
+++ multiple_pointers/averagePair.js
@@ -0,0 +1,21 @@
+// Determine if there is a pair of values in the array
+// where the average of the pair equals the target average
+
+function averagePair(arr, targetAvg) {
+  let firstPointer = 0;
+  let secPointer = arr.length - 1;
+
+  arr.sort((a,b) => a > b);
+
+  while (firstPointer < secPointer) {
+    let avg = (arr[firstPointer] + arr[secPointer]) / 2;
+    console.log(arr[firstPointer], arr[secPointer])
+    if (avg === targetAvg) return true
+    else if (avg > targetAvg) secPointer--
+    else firstPointer++;
+  }
+
+  return false;
+}
+
+console.log(averagePair([1,2,3,4], 4))
