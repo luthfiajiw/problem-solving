a84889ca74699b1e191b9f132dbfe791b0f54b6e
Luthfi Aji W <luthfi.ajiw@gmail.com>
2020-02-15 17:35:02 +0700

    binarySearch

---
+++ search/binarySearch.js
@@ -0,0 +1,20 @@
+function binarySearch(sortedArr, query) {
+  let start = 0;
+  let end = sortedArr.length - 1;
+  let middle = Math.floor((start + end) / 2);
+
+  sortedArr.sort((a,b) => a > b);
+
+  while (sortedArr[middle] !== query && start <= end) {
+    if (sortedArr[middle] < query) {
+      end = middle - 1;
+    } else {
+      start = middle + 1;
+    }
+    middle = Math.floor((start + end) / 2);
+  }
+
+  return sortedArr[middle] === query ? middle : -1;
+}
+
+console.log(binarySearch([2,4,6,9,12,15,18], 5));
